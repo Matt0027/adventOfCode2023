a33bb45a7b930f54975ac7610f5f99b77bf42220
Matt0027 <152666898+Matt0027@users.noreply.github.com>
2023-12-02 16:57:54 -0500

    commit
    Day 2 complete

---
+++ AdventOfCode2023/Day2.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2023
+{
+    internal class Day2
+    {
+        internal static int Day2Main (string[] inputArray)
+        {
+            var sum = 0;
+            const int maxRedBalls = 12;
+            const int maxGreenBalls = 13;
+            const int maxBlueBalls = 14;
+
+            foreach (var line in inputArray)
+            {
+                var semicolonSplit = line.Split(':');
+                var gameNum = Convert.ToInt32(semicolonSplit[0].Split(' ')[1]);
+                var randDraws = semicolonSplit[1].Split(';');
+
+                var impossibleDraw = false;
+
+                foreach (var draw in randDraws)
+                {
+                    var numColor = draw.Split(',');
+
+                    foreach (var color in numColor)
+                    {
+                        var numOfBalls = Convert.ToInt32(color.Trim().Split(' ')[0]);
+                        var colorOfBalls = color.Trim().Split(' ')[1];
+
+                        if (colorOfBalls == "red")
+                        {
+                            if (numOfBalls > maxRedBalls)
+                                impossibleDraw = true;
+                        }
+                        else if (colorOfBalls == "green")
+                        {
+                            if (numOfBalls > maxGreenBalls)
+                                impossibleDraw = true;
+                        }
+                        else if (colorOfBalls == "blue")
+                        {
+                            if (numOfBalls > maxBlueBalls)
+                                impossibleDraw = true;
+                        }
+                        else
+                            impossibleDraw = true;
+
+                        if (impossibleDraw)
+                            break;
+                    }
+                }
+
+                if (!impossibleDraw)
+                    sum += gameNum;
+            }
+
+            return sum;
+        }
+
+        internal static int Day2Main2 (string[] inputArray)
+        {
+            var sum = 0;
+            //const int maxRedBalls = 12;
+            //const int maxGreenBalls = 13;
+            //const int maxBlueBalls = 14;
+
+            foreach (var line in inputArray)
+            {
+                var semicolonSplit = line.Split(':');
+                var gameNum = Convert.ToInt32(semicolonSplit[0].Split(' ')[1]);
+                var randDraws = semicolonSplit[1].Split(';');
+
+                //var impossibleDraw = false;
+                var minRedBalls = 0;
+                var minBlueBalls = 0;
+                var minGreenBalls = 0;
+
+                foreach (var draw in randDraws)
+                {
+                    var numColor = draw.Split(',');
+
+                    foreach (var color in numColor)
+                    {
+                        var numOfBalls = Convert.ToInt32(color.Trim().Split(' ')[0]);
+                        var colorOfBalls = color.Trim().Split(' ')[1];
+
+                        if (colorOfBalls == "red")
+                        {
+                            if (numOfBalls > minRedBalls)
+                                minRedBalls = numOfBalls;
+                        }
+                        else if (colorOfBalls == "green")
+                        {
+                            if (numOfBalls > minGreenBalls)
+                                minGreenBalls = numOfBalls;
+                        }
+                        else if (colorOfBalls == "blue")
+                        {
+                            if (numOfBalls > minBlueBalls)
+                                minBlueBalls = numOfBalls;
+                        }
+                        else
+                            throw new InvalidDataException("The color is not red, blue, or green for Game " + gameNum);
+                    }
+                }
+
+                sum += (minRedBalls * minGreenBalls * minBlueBalls);
+            }
+
+            return sum;
+        }
+    }
+}
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -9,17 +9,27 @@ namespace AdventOfCode2023
         {
             // Day 1
 
-            //var exampleInput = File.ReadAllLines(@"C:\Users\Mine\Documents\AoC Day1 Example.txt");
+            //var exampleInput = File.ReadAllLines(@"C:\Users\Mine\Documents\AdventOfCodeInput\AoC Day1 Example.txt");
             //Console.WriteLine("The sum is: " + Day1.SumFirstAndLastNum(exampleInput));
 
-            var dayOneInput = File.ReadAllLines(@"C:\Users\Mine\Documents\AoC Day 1 Input.txt");
+            //var dayOneInput = File.ReadAllLines(@"C:\Users\Mine\Documents\AdventOfCodeInput\AoC Day 1 Input.txt");
             //Console.WriteLine("The sum is: " + Day1SumFirstAndLastNum(dayOneInput));
 
-            //var dayOneExampleTwoInput = File.ReadAllLines(@"C:\Users\Mine\Documents\AoC Day1 Example2.txt");
+            //var dayOneExampleTwoInput = File.ReadAllLines(@"C:\Users\Mine\Documents\AdventOfCodeInput\AoC Day1 Example2.txt");
             //Console.WriteLine("The sum is: " + Day1SumFirstAndLastNum2(dayOneExampleTwoInput));
 
-            Console.WriteLine("The sum is: " + Day1.SumFirstAndLastNum2(dayOneInput));
-            
+            //Console.WriteLine("The sum is: " + Day1.SumFirstAndLastNum2(dayOneInput));
+
+            //-----------------------------------------------------------------------------------------------
+
+            // Day 2
+            //var dayTwoExampleInput = File.ReadAllLines(@"C:\Users\Mine\Documents\AdventOfCodeInput\AoC Day2 Example.txt");
+            //Console.WriteLine(Day2.Day2Main(dayTwoExampleInput));
+            //Console.WriteLine(Day2.Day2Main2(dayTwoExampleInput));
+
+            var dayTwoInput = File.ReadAllLines(@"C:\Users\Mine\Documents\AdventOfCodeInput\AoC Day2 Input.txt");
+            Console.WriteLine(Day2.Day2Main(dayTwoInput));
+            Console.WriteLine(Day2.Day2Main2(dayTwoInput));
         }
     }
 }
